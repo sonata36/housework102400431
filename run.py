d3abe9cfbcce5796b5942a226f144f737a1ce3ad
taotaozi <1573888461@qq.com>
2026-09-21 21:38:18 +0800

    fix: 修改run.py缩进，适配render部署

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 """Run the local development server from PyCharm or a terminal."""
-
 from app import create_app
-
 
 app = create_app()
 
-
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
